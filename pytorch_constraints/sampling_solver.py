5af1b8923b9a5722e522b2dd0389886763403be8
Kareem Ahmed <kareem.yousrii@gmail.com>
2020-08-01 17:30:23 -0700

    Update SamplingSolver to fix loss and extend functionality to non-binary classification

--- pytorch_constraints/sampling_solver.py
+++ pytorch_constraints/sampling_solver.py
@@ -26,31 +26,27 @@ class SamplingSolver(Solver):
     def loss(self, logits):
         sampler = self.sample(logits)
         samples = [next(sampler) for _ in range(self.num_samples)]
-        # samples = [self.sample(logits) for s in range(self.num_samples)]
 
         log_probs = torch.log_softmax(logits, dim=-1)
-        satis_losses = map(lambda sample: decoding_loss(sample, log_probs),
-                           filter(self.cond, samples))
-        satis_losses = tuple(satis_losses)
-        satis_loss = torch.stack(tuple(satis_losses)).logsumexp(dim=0)\
-            if satis_losses else 0
+        satis_losses = tuple(map(lambda sample: decoding_loss(sample, log_probs),
+                           filter(self.cond, samples)))
+        satis_loss = torch.stack(satis_losses).logsumexp(dim=0)\
+            if satis_losses else torch.tensor(0.0)
 
-        viol_losses = map(lambda sample: decoding_loss(sample, log_probs),
-                          filter(lambda v: not self.cond(v), samples))
-        viol_losses = tuple(viol_losses)
-        viol_loss = torch.stack(tuple(viol_losses)).logsumexp(dim=0)\
-            if viol_losses else 0
+        viol_losses = tuple(map(lambda sample: decoding_loss(sample, log_probs),
+                          filter(lambda v: not self.cond(v), samples)))
+        viol_loss = torch.stack(viol_losses).logsumexp(dim=0)\
+            if viol_losses else torch.tensor(0.0)
 
-        # log_prob_satis = satis_loss - torch.logsumexp(satis_loss, viol_loss)
-        # log_prob_viol = viol_loss - torch.logsumexp(satis_loss, viol_loss)
-        # return -log_prob_satis
-        return viol_loss - satis_loss
+        log_prob_satis = satis_loss - torch.logsumexp(torch.stack((satis_loss, viol_loss)), dim=0)
+        return -log_prob_satis
 
 
 class WeightedSamplingSolver(SamplingSolver):
     def sample(self, logits):
-        '''Sample a decoding of variables.'''
-        # We're only interested in p, and not 1-p
-        probs = torch.softmax(logits, dim=-1)[:, -1]
+        '''Sample a decoding of variables from a multinomial distribution parameterized by network probabilities'''
+
+        probs = torch.softmax(logits, dim=-1)
+
         while(True):
-            yield torch.bernoulli(probs).long()
+            yield torch.multinomial(probs, num_samples=1, replacement=True).transpose_(0,1).flatten()
