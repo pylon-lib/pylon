2da2a5a748b729e920be237cb8b874cc0728e74d
Tao Li <tli_march@hotmail.com>
2020-09-03 13:24:09 -0600

    simplified basic tests a bit

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -85,6 +85,43 @@ def test_xor_multi(net_multi):
         assert success == num_tries
 
 
+def test_or_binary(net_binary):
+    solvers = get_solvers(num_samples=10)
+    for solver in solvers:
+        num_tries = 5  # since it's random
+        success = 0
+        for i in range(num_tries):
+            cons = constraint(lambda y: y[0] or y[1], solver)
+
+            net, y0 = train(net_binary, cons)
+            x = torch.tensor([1.0])
+            y = F.softmax(net(x), dim=-1)
+
+            if not (y[0, 0] > 0.6 and y[1, 0] > 0.6):
+                success += 1
+
+        assert success == num_tries
+
+
+def test_or_multi(net_multi):
+    solvers = get_solvers(num_samples=20)
+    for solver in solvers:
+        print("Testing", type(solver).__name__)
+        num_tries = 5  # since it's random
+        success = 0
+        for i in range(num_tries):
+            cons = constraint(lambda y: y[0] or y[1], solver)
+
+            net, y0 = train(net_multi, cons)
+            x = torch.tensor([1.0])
+            y = F.softmax(net(x), dim=-1)
+
+            if not (y[0, 0] > 0.6 and y[1, 0] > 0.6):
+                success += 1
+
+        assert success == num_tries
+
+
 def test_eq_multi(net_multi):
     solvers = get_solvers(num_samples=20)
     for solver in solvers:
@@ -122,24 +159,6 @@ def test_neq_multi(net_multi):
 
         assert success == num_tries
 
-def test_logical_and_binary(net_binary):
-    solvers = get_solvers(num_samples=10)
-    for solver in solvers:
-        num_tries = 5  # since it's random
-        success = 0
-        for i in range(num_tries):
-            cons = constraint(lambda y: y[0] and y[1], solver)
-
-            net, y0 = train(net_binary, cons)
-            x = torch.tensor([1.0])
-            y = F.softmax(net(x), dim=-1)
-
-            if y[0, 0] < 0.25:
-                success += 1
-            assert y[1, 0] < 0.25
-
-        assert success == num_tries
-
 
 def test_logical_and_multi(net_multi):
     solvers = get_solvers(num_samples=20)
@@ -157,25 +176,6 @@ def test_logical_and_multi(net_multi):
             if y[0, 0] < 0.2:
                 success += 1
             assert y[1, 0] < 0.6
-
-        assert success == num_tries
-
-# TODO, currently only implemented in tnorms solvers
-def test_implication_binary(net_multi):
-    solvers = get_tnorm_solvers()
-    for solver in solvers:
-        print("Testing", type(solver).__name__)
-        num_tries = 5  # since it's random
-        success = 0
-        for i in range(num_tries):
-            cons = constraint(lambda y: y[0] <= y[1], solver)
-
-            net, y0 = train(net_multi, cons)
-            x = torch.tensor([1.0])
-            y = F.softmax(net(x), dim=-1)
-
-            if y[0, 1:].max() < y[1, 1:].max():
-                success += 1
 
         assert success == num_tries
 
@@ -198,25 +198,6 @@ def test_implication_multi(net_multi):
 
         assert success == num_tries
 
-
-# TODO, sigmoidal implication are currently only implemented in tnorm solvers
-def test_s_implication_binary(net_multi):
-    solvers = get_tnorm_solvers()
-    for solver in solvers:
-        print("Testing", type(solver).__name__)
-        num_tries = 5  # since it's random
-        success = 0
-        for i in range(num_tries):
-            cons = constraint(lambda y: y[0].s_implies(y[1]), solver)
-
-            net, y0 = train(net_multi, cons)
-            x = torch.tensor([1.0])
-            y = F.softmax(net(x), dim=-1)
-
-            if y[0, 1:].max() < y[1, 1:].max():
-                success += 1
-
-        assert success == num_tries
 
 # TODO, sigmoidal implication are currently only implemented in tnorm solvers
 def test_s_implication_multi(net_multi):
